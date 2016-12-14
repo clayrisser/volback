f70e888ae85ebc13745ef3247d76d710dc521716
Jam Risser <jamrizzi@gmail.com>
2016-12-14 12:39:31 -0600

    Changed some environment variables

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -4,6 +4,6 @@ if [ "$ALLOW_SOURCE_MISMATCH" == "true" ]; then
     _ALLOW_SOURCE_MISMATCH="--allow-source-mismatch "
 fi
 
-eval "(echo $PASSPHRASE; echo $PASSPHRASE) | duplicity ""$BACKUP_TYPE ""$_ALLOW_SOURCE_MISMATCH""--full-if-older-than $OLDER_THAN $BACKUP_DIR $TARGET_URL"
+eval "(echo $PASSPHRASE; echo $PASSPHRASE) | duplicity ""$BACKUP_TYPE ""$_ALLOW_SOURCE_MISMATCH""--full-if-older-than $FULL_IF_OLDER_THAN $BACKUP_DIR $TARGET_URL"
 
 bash /scripts/clean.sh
