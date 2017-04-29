6727174bdaf93ccfacd6f75ce946a0c13a57e926
Jam Risser <jam@jamrizzi.com>
2017-04-29 14:52:21 -0500

    Added fundamental borg routes

--- ident/src/pkg/borg.py
+++ ident/src/pkg/borg.py
@@ -1,11 +1,31 @@
 import os
 
 class Borg:
-    def __init__(self):
-        return
+    def __init__(self, borg_dir):
+        self.borg_dir = borg_dir
 
     def __run(self, command):
-        return os.environ('borg ' + command)
+        return os.popen('cd ' + self.borg_dir + ' && ' + command).read()
 
-    def list(self):
-        res = __run(self, 'list')
+    def __borg(self, command):
+        return self.__run('borg ' + command)
+
+    def get_containers(self):
+        return os.listdir(self.borg_dir)
+
+    def get_container_backups(self, container):
+        string = self.__borg('list ' + container)
+        backups = string.split('\n')
+        del backups[len(backups) - 1]
+        sanatized_backups = list()
+        for backup in backups:
+            backup = backup.split(' ')[0]
+            sanatized_backups.append(backup)
+        return sanatized_backups
+
+    def get_containers_backups(self):
+        containers = self.get_containers()
+        containers_backups = {}
+        for container in containers:
+            containers_backups[container] = self.get_container_backups(container)
+        return containers_backups
--- ident/src/server.py
+++ ident/src/server.py
@@ -1,4 +1,5 @@
 import os
+from pkg.borg import Borg
 from flask import Flask
 from flask_restful import Resource, Api
 
@@ -10,12 +11,31 @@ options = {
 
 app = Flask(__name__)
 api = Api(app)
+borg = Borg('/backup')
 
 class HelloWorld(Resource):
     def get(self):
-        return {'hello': 'world'}
+        return {'hello': 'ident'}
+
+class Containers(Resource):
+    def get(self):
+        containers = borg.get_containers()
+        return containers
+
+class ContainerBackups(Resource):
+    def get(self, container):
+        container_backups = borg.get_container_backups(container)
+        return container_backups
+
+class ContainersBackups(Resource):
+    def get(self):
+        containers_backups = borg.get_containers_backups()
+        return containers_backups
 
 api.add_resource(HelloWorld, '/')
+api.add_resource(Containers, '/containers')
+api.add_resource(ContainerBackups, '/container-backups/<container>')
+api.add_resource(ContainersBackups, '/containers-backups')
 
 if __name__ == '__main__':
     app.run(
